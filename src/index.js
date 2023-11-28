a3d020349405537b1c1e11b6af9e856f3f6a7501
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-11-27 23:22:19 -0500

    following tutorial odin project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash'
+import myName from './myName'
+import './style.css'
 
 function component() {
     const element = document.createElement('div')
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello')
+
+    element.textContent = myName('Cody')
 
     return element
 
